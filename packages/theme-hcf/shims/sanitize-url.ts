0b3291a11021fd206a8de6ea5b78842b771b34e1
qq2236639958 <2236639958@qq.com>
2026-09-25 14:40:39 +0800

    add

--- packages/theme-hcf/shims/sanitize-url.ts
+++
@@ -1,5 +0,0 @@
-export function sanitizeUrl(value: string): string {
-  return value
-}
-
-export default sanitizeUrl
--- packages/vitepress-theme-hcf/shims/dayjs.ts
+++
@@ -1,6 +0,0 @@
-import dayjsModule from 'dayjs/dayjs.min.js'
-
-const dayjs = (dayjsModule as typeof import('dayjs')).default ?? dayjsModule
-
-export default dayjs
-export * from 'dayjs/dayjs.min.js'
